8b0d97be0ccb68bcc1945e28b2f48d3488ea4cb2
caio <caiomarcos@gmail.com>
2019-11-02 23:48:55 -0300

    Calculates RMS, kurtosis and skewness For a specific folder, calculates RMS, kurtosis and skewness for each column for each file and makes a .csv f for each with calculated values

--- trabalho.py
+++ trabalho.py
@@ -32,7 +32,7 @@ from scipy.stats import skew
 #        rmsee = 1/
 #%%
 #Set dir where data is
-data_dir = '1st_test'
+data_dir = '2nd_test'
 #count the number of files
 file_list = os.listdir(data_dir)
 number_files = len(file_list)
@@ -42,47 +42,40 @@ df_kurt = pd.DataFrame()
 df_skw = pd.DataFrame()
 #%%
 #for each file in folder
-for filename in os.listdir(data_dir):
-    print(filename)
-    #make a dataset with column values in the file
-    dataset = pd.read_csv(os.path.join(data_dir, filename), sep='\t', usecols=[0])
-    #calculate rms for those values
-    rms = np.sqrt(np.mean(dataset**2))
-    #make it a dataframe object
-    rms = pd.DataFrame(np.array(rms).T)
-    #calculate kurtosis for those values
-    kurt = kurtosis(dataset)
-    #make it a dataframe object
-    kurt = pd.DataFrame(np.array(kurt).T)
-    #calculate skewness for those values
-    skw = skew(dataset)
-    #make it a dataframe object
-    skw = pd.DataFrame(np.array(skw).T)
-    #append rms to the rms values dataset
-    df_rms = df_rms.append(rms, ignore_index=True)
-    #append kurtosis to the kurtosis values dataset
-    df_kurt = df_kurt.append(kurt, ignore_index=True)
-    #append skewness to the skewness values dataset
-    df_skw = df_skw.append(skw, ignore_index=True)
-#%%
-#plot RMS
-df_rms.plot(figsize = (24,12))
-plt.show()
-#plot RMS
-df_kurt.plot(figsize = (24,12))
-plt.show()
-#plot RMS
-df_skw.plot(figsize = (24,12))
-plt.show()
-#%%
-#make csv out of the calculated values
-#create dataframe with the correct size
-dataset = pd.DataFrame(index=range(2113), columns=range(3))
-#name dataframe columns
-dataset.columns = ['RMS', 'kurtosis', 'skewness']
-#assign values to columns
-dataset['RMS'] = df_rms
-dataset['kurtosis'] = df_kurt
-dataset['skewness'] = df_skw
-#create .csv
-dataset.to_csv('1st_test-ch1.csv')
+for channel in [0, 1, 2, 3]:
+    for filename in os.listdir(data_dir):
+        print(filename)
+        #make a dataset with column values in the file
+        dataset = pd.read_csv(os.path.join(data_dir, filename), sep='\t', header=None, usecols=[channel])
+        #calculate rms for those values
+        rms = np.sqrt(np.mean(dataset**2))
+        #make it a dataframe object
+        rms = pd.DataFrame(np.array(rms).T)
+        #calculate kurtosis for those values
+        kurt = kurtosis(dataset)
+        #make it a dataframe object
+        kurt = pd.DataFrame(np.array(kurt).T)
+        #calculate skewness for those values
+        skw = skew(dataset)
+        #make it a dataframe object
+        skw = pd.DataFrame(np.array(skw).T)
+        #append rms to the rms values dataset
+        df_rms = df_rms.append(rms, ignore_index=True)
+        #append kurtosis to the kurtosis values dataset
+        df_kurt = df_kurt.append(kurt, ignore_index=True)
+        #append skewness to the skewness values dataset
+        df_skw = df_skw.append(skw, ignore_index=True)
+    #make csv out of the calculated values
+    #create dataframe with the correct size
+    dataset = pd.DataFrame(index=range(number_files), columns=range(3))
+    #name dataframe columns
+    dataset.columns = ['RMS', 'kurtosis', 'skewness']
+    #assign values to columns
+    dataset['RMS'] = df_rms
+    dataset['kurtosis'] = df_kurt
+    dataset['skewness'] = df_skw
+    #create .csv
+    name = '3rd_or_4th_test_-ch'
+    name = name + str(channel+1)
+    name = name + '.csv'
+    dataset.to_csv(name)
